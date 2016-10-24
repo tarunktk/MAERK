f7042192f189aada7eedd19edfdec3e4f953c0c1
Tarun Kondapalli <tarunkondapalli@Taruns-MacBook-Air.local>
2016-10-24 15:30:39 -0500

    added angular material 2.0

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('maerkApp', ['maerkApp.auth', 'maerkApp.admin', 'maerkApp.constants', 'ngCookies',
-    'ngResource', 'ngSanitize', 'ui.router', 'validation.match'
+    'ngResource', 'ngSanitize', 'ui.router', 'validation.match','ngMaterial'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');

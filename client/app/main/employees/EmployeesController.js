f7413e989717314ecd34f52528a00108ec1e5d40
Tarun Kondapalli <tarunkondapalli@Taruns-MacBook-Air.local>
2016-10-28 09:55:05 -0500

    responsive login page with angular material

--- client/app/main/employees/EmployeesController.js
+++ client/app/main/employees/EmployeesController.js
@@ -3,125 +3,121 @@
 
    angular.module('maerkApp')
      .controller('EmployeesController', function($scope, $mdToast){
-       $scope.deleteRowCallback = function(rows){
-            $mdToast.show(
-                $mdToast.simple()
-                    .content('Deleted row id(s): '+rows)
-                    .hideDelay(3000)
-            );
-        };
-        $scope.saveRowCallback = function(row){
-            $mdToast.show(
-                $mdToast.simple()
-                    .content('Row changed to: '+row)
-                    .hideDelay(3000)
-            );
-        };
-        $scope.selectedRowCallback = function(rows){
-            $mdToast.show(
-                $mdToast.simple()
-                    .content('Selected row id(s): '+rows)
-                    .hideDelay(3000)
-            );
-        };
+      //  $scope.deleteRowCallback = function(rows){
+      //       $mdToast.show(
+      //           $mdToast.simple()
+      //               .content('Deleted row id(s): '+rows)
+      //               .hideDelay(3000)
+      //       );
+      //   };
+      //   $scope.saveRowCallback = function(row){
+      //       $mdToast.show(
+      //           $mdToast.simple()
+      //               .content('Row changed to: '+row)
+      //               .hideDelay(3000)
+      //       );
+      //   };
+      //   $scope.selectedRowCallback = function(rows){
+      //       $mdToast.show(
+      //           $mdToast.simple()
+      //               .content('Selected row id(s): '+rows)
+      //               .hideDelay(3000)
+      //       );
+      //   };
 
        $scope.employeeList = [
            {
                employee_id: 1,
-               firstName: 'aaa',
-               lastName: 'dvdavda',
-               client: 'ldkvdlkvb',
-               skills: 'lwkdvzdlkv',
-               recruiter: 'labclbl',
-               revenue: 3200
+               firstName: 'Peter',
+               lastName: 'Griffin',
+               client: 'Pawtucket Brewery',
+               skills: 'HAHAHAHAHAHA!!',
+               recruiter: 'Angela',
+               revenue: 50000
            },
            {
                employee_id: 2,
-               firstName: 'andc;zdnv;',
-               lastName: 'lkefbdlksf',
-               client: 'kfvakfjb',
-               skills: 'cb,cbscb',
-               recruiter:'lkdnvlksdn',
-
-               revenue: '14%'
+               firstName: 'Louis',
+               lastName: 'Griffin',
+               client: 'Peter',
+               skills: 'Groceries',
+               recruiter:'Peter',
+               revenue: 50000
            },
            {
                employee_id: 3,
-               firstName: 'Frozen joghurt',
-               lastName: 159,
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
-               hours: 87,
-               revenue: '14%'
+               firstName: 'Stewie',
+               lastName: 'Griffin',
+               client: 'ISIS',
+               skills: 'Singing & beating shit out of brain',
+               recruiter: 'Louis',
+               revenue: 50000
            },
            {
                employee_id: 4,
-               firstName: 'Frozkwefnk',
-               lastName: 'ldkfndslf',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
-               revenue: '14%'
+               firstName: 'Brain',
+               lastName: 'Griffin',
+               client:    'Self',
+               skills:   'Writes Stuff',
+               recruiter: 'Carter Pewterschmidt',
+               revenue: 50000
            },
            {
                employee_id: 5,
-               firstName: 'lksnflksdnv',
-               lastName: 'ldknds',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
+               firstName: 'Megan',
+               lastName: 'Griffin',
+               client: 'Superstore USA',
+               skills: 'Shut up MEG!!',
+               recruiter: 'Mr.Penisburg',
 
-               revenue: '14%'
+               revenue: 50000
            },
            {
                employee_id: 6,
-               firstName: 'knfdslknfrt',
-               lastName: 'jdkfdkjsb',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
-               revenue: '14%'
+               firstName: 'Chris',
+               lastName: 'Griffin',
+               client: 'Quahog Mini-Mart',
+               skills: 'Football',
+               recruiter: 'Carl',
+               revenue: 50000
            },
            {
                employee_id: 7,
-               firstName: 'Fe[pfe[pfkew[f]]]',
-               lastName: 'fhoeufewbf',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
+               firstName: 'Adam',
+               lastName: 'West',
+               client: 'Quahog state',
+               skills: 'Mayoring skills.:D',
+               recruiter: 'Democracy???',
 
-               revenue: '14%'
+               revenue: 50000
            },
            {
                employee_id: 8,
-               firstName: 'lksvsdkvd',
-               lastName: 'kjbfdskjbfkdjsbfd',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
+               firstName: 'Hebert',
+               lastName: 'The Pervert',
+               client: 'young boys',
+               skills: 'luring teens with candy',
+               recruiter: 'unknown',
 
-               revenue: '14%'
+               revenue: 5000000
            },
            {
                employee_id: 9,
-               firstName: 'sljks;dkjvsdkjv',
-               lastName: 'dndskljn',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
-
-               revenue: '14%'
+               firstName: 'Glenn :(',
+               lastName: 'Quagmire',
+               client: 'American airlines',
+               skills: 'Pilot/HandcuffPicker',
+               recruiter: 'unknown',
+               revenue: 1400000
            },
            {
                employee_id: 10,
-               firstName: 'nnnnnnn',
-               lastName: 'mmmmmmm',
-               client: 6.0,
-               skills: 24,
-               recruiter: 4.0,
-
-               revenue: '14%'
+               firstName: 'Cleveland',
+               lastName: 'Brown',
+               client: 'Unemployed',
+               skills: 'no no no no no',
+               recruiter: 'i give up',
+               revenue: 50000
            }
        ];
    });

9ae6dd24c27625046b57b8c672c068751565d24e
Tarun Kondapalli <tarunkondapalli@Taruns-MacBook-Air.local>
2016-11-01 10:40:26 -0500

    added authentication

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('maerkApp', ['maerkApp.auth', 'maerkApp.admin', 'maerkApp.constants', 'ngCookies',
-    'ngResource', 'ngSanitize', 'ui.router', 'validation.match','ngMaterial', 'mdDataTable',
+    'ngResource', 'ngSanitize', 'ui.router', 'validation.match','ngMaterial', 'mdDataTable'
   ])
   .config(function($stateProvider,$urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/login');
--- server/api/employees/index.js
+++ server/api/employees/index.js
@@ -7,8 +7,8 @@ import * as auth from '../../auth/auth.service';
 
 var router = Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
+router.get('/', auth.hasRole('admin'),controller.index);
+router.get('/:id', auth.hasRole('admin'),controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'),controller.update);
 module.exports = router;
